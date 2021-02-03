15552bdf9523afb5b99cdd8457a7b9a498774844
mrAzay <furoxaz@gmail.com>
2021-02-04 00:49:08 +0200

    s

--- mail.php
+++ mail.php
@@ -1,13 +1,13 @@
-<?PHP 
-$to = "saunflash@yandex.ru";
-$tema = "Заявка на заказ"; 
+<?PHP
+$to = "furoxaz@gmail.com";
+$tema = "Заявка на заказ";
 $message = "Имя: ".$_POST['name']."<br>";
-$message .= "Номер телефона: ".$_POST['phone']."<br>"; 
-$message .= "Почта: ".$_POST['mail']."<br>"; 
+$message .= "Номер телефона: ".$_POST['phone']."<br>";
+$message .= "Почта: ".$_POST['mail']."<br>";
 
 
-$headers  = 'MIME-Version: 1.0' . "\r\n"; 
-  $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; 
-mail($to, $tema, $message, $headers); 
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+  $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+mail($to, $tema, $message, $headers);
 
 ?>
